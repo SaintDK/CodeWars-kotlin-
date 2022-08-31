22336321e1c1296255cafe2c35655b824112807f
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-08-31 07:30:35 -0400

    commit

--- src/main/kotlin/kuy_8/Find_the_smallest_integer_in_the_array/Test.kt
+++ src/main/kotlin/kuy_8/Find_the_smallest_integer_in_the_array/Test.kt
@@ -2,8 +2,8 @@ package kuy_8.Find_the_smallest_integer_in_the_array
 
 class SmallestIntegerFinder {
 
-    fun findSmallestInt(nums: List<Int>): Int? {
-        return nums.minOrNull()
+    fun findSmallestInt(nums: List<Int>): Int {
+        return nums.minOf { it }
     }
 
 }
