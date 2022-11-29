b858b58bba91836b74c0358784df793712473ae3
ivany <ivan.yakimishin.00@mail.ru>
2022-11-29 15:31:20 +0300

    Commit

---
+++ src/main/kotlin/kuy_8/Enumerable_Magic_Does_My_List_Include_This/TestExample.kt
@@ -0,0 +1,13 @@
+package kuy_8.Enumerable_Magic_Does_My_List_Include_This
+
+import junit.framework.TestCase.assertEquals
+import kuy_8.Enumerable_Magic_3_Does_My_List_Include_This.include
+import org.junit.Test
+
+class TestExample {
+    @Test
+    fun `Basic Tests`() {
+        assertEquals(true, include(intArrayOf(1,2,3,4), 2))
+        assertEquals(false, include(intArrayOf(1,2,4,5), 3))
+    }
+}
---
+++ src/main/kotlin/kuy_8/Enumerable_Magic_Does_My_List_Include_This/include.kt
@@ -0,0 +1,3 @@
+package kuy_8.Enumerable_Magic_Does_My_List_Include_This
+
+fun include(arr: IntArray, item : Int) = item in arr
