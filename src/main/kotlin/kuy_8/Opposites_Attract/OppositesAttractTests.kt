119b5f7b2562416d4182b4cf838845f9a7dfaa92
windows <SaintDK>
2022-12-22 21:52:02 +0300

    commit

---
+++ src/main/kotlin/kuy_8/Opposites_Attract/OppositesAttractTests.kt
@@ -0,0 +1,16 @@
+package kuy_8.Opposites_Attract
+
+import org.junit.Assert.*
+import org.junit.Test
+import java.util.*
+
+class OppositesAttractTests {
+
+    @Test
+    fun exampleTests() {
+        assertTrue(loveFun(1, 4))
+        assertFalse(loveFun(2, 2))
+        assertTrue(loveFun(0, 1))
+        assertFalse(loveFun(0, 0))
+    }
+}
---
+++ src/main/kotlin/kuy_8/Opposites_Attract/loveFun.kt
@@ -0,0 +1,3 @@
+package kuy_8.Opposites_Attract
+
+fun loveFun(flowerA: Int, flowerB: Int): Boolean = (flowerA + flowerB) %2 == 1
