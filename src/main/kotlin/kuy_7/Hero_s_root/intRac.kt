44f09a0032bde8d7cea7a1ab64f60859af208aa2
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-12-27 14:53:22 +0300

    Update intRac.kt

--- src/main/kotlin/kuy_7/Hero_s_root/intRac.kt
+++ src/main/kotlin/kuy_7/Hero_s_root/intRac.kt
@@ -1,7 +1,10 @@
 package kuy_7.Hero_s_root
 
 fun intRac(n:Long, guess:Long):Long {
+    
     val newGuess = (n / guess + guess) / 2
+    
     if( newGuess == guess ) return(1)
+    
     return intRac(n, newGuess)+1
 }
