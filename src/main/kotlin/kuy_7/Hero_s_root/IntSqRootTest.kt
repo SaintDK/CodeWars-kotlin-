a957638f6c2bbb8f2d3785366b62af355118d977
windows <SaintDK>
2022-12-27 14:46:54 +0300

    commit

---
+++ src/main/kotlin/kuy_7/Hero_s_root/IntSqRootTest.kt
@@ -0,0 +1,15 @@
+package kuy_7.Hero_s_root
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class IntSqRootTest {
+    @Test
+    fun test0() {
+        println("Fixed Tests")
+        assertEquals(4, intRac(25, 1));
+        assertEquals(3, intRac(125348, 300));
+
+    }
+
+}
