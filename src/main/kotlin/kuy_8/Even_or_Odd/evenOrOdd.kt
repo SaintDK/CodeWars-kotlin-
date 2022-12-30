db3c73c3a7b6116d1976724accc67419f9f10453
ivany <ivan.yakimishin.00@mail.ru>
2022-12-30 18:49:26 +0300

    Commit

--- src/main/kotlin/kuy_8/Even_or_Odd/evenOrOdd.kt
+++ src/main/kotlin/kuy_8/Even_or_Odd/evenOrOdd.kt
@@ -1,3 +1,3 @@
 package kuy_8.Even_or_Odd
 
-fun evenOrOdd(number: Int): String = if (number % 2 == 0) { "Even" }else "Odd"
+fun evenOrOdd(number: Int): String = if (number % 2 == 0) "Even" else "Odd"
