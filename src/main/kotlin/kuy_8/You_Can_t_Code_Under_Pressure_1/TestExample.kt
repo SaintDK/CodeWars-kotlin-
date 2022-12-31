cda5b82230dbc135a1eab6dd7102f1153b45de6b
ivany <ivan.yakimishin.00@mail.ru>
2022-12-31 09:57:55 +0300

    Commit

---
+++ src/main/kotlin/kuy_8/You_Can_t_Code_Under_Pressure_1/TestExample.kt
@@ -0,0 +1,11 @@
+package kuy_8.You_Can_t_Code_Under_Pressure_1
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class TestExample {
+    @Test
+    fun multiply() {
+        assertEquals(4, doubleInteger(2), "Nope!")
+    }
+}
---
+++ src/main/kotlin/kuy_8/You_Can_t_Code_Under_Pressure_1/doubleInteger.kt
@@ -0,0 +1,4 @@
+package kuy_8.You_Can_t_Code_Under_Pressure_1
+
+
+fun doubleInteger(i:Int):Int = i * 2
