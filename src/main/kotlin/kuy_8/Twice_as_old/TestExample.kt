ca781b188811408f4d9a1e30149efc607973458b
ivany <ivan.yakimishin.00@mail.ru>
2023-01-04 19:47:59 +0300

    Commit

---
+++ src/main/kotlin/kuy_8/Twice_as_old/TestExample.kt
@@ -0,0 +1,6 @@
+package kuy_8.Twice_as_old
+
+fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int): Int {
+    val diff = dadYearsOld - (sonYearsOld * 2)
+    return Math.abs(diff)
+}
---
+++ src/main/kotlin/kuy_8/Twice_as_old/twiceAsOld.kt
@@ -0,0 +1,15 @@
+package kuy_8.Twice_as_old
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class TestExample {
+    @Test
+    fun testFixed() {
+        assertEquals(22, twiceAsOld(36,7))
+        assertEquals(5, twiceAsOld(55,30))
+        assertEquals(0, twiceAsOld(42,21))
+        assertEquals(20, twiceAsOld(22,1))
+        assertEquals(29, twiceAsOld(29,0))
+    }
+}
