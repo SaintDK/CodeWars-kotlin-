a798ad2a21a87e0ad23f8ff7b1a340a0242b53ff
ivany <ivan.yakimishin.00@mail.ru>
2023-02-25 13:00:33 +0300

    Commit

---
+++ src/main/kotlin/kuy_7/Two_Oldest_Ages/TestTwoOldestAges.kt
@@ -0,0 +1,15 @@
+package kuy_7.Two_Oldest_Ages
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class TestTwoOldestAges : StringSpec() {
+    init {
+        "twoOldestAges(listOf(1,5,87,45,8,8)) should be listOf(45, 87)" {
+            twoOldestAges(listOf(1,5,87,45,8,8)) shouldBe listOf(45, 87)
+        }
+        "twoOldestAges(listOf(6,5,83,5,3,18)) should be listOf(18, 83)" {
+            twoOldestAges(listOf(6,5,83,5,3,18)) shouldBe listOf(18, 83)
+        }
+    }
+}
---
+++ src/main/kotlin/kuy_7/Two_Oldest_Ages/twoOldestAges.kt
@@ -0,0 +1,3 @@
+package kuy_7.Two_Oldest_Ages
+
+fun twoOldestAges(ages: List<Int>) = ages.sorted().takeLast(2)
