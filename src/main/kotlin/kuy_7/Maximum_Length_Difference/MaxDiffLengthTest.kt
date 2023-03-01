a9a90aee27f533aaacd29bb96841e87287c5fd06
ivany <ivan.yakimishin.00@mail.ru>
2023-03-01 14:05:48 +0300

    Commit

---
+++ src/main/kotlin/kuy_7/Maximum_Length_Difference/MaxDiffLengthTest.kt
@@ -0,0 +1,18 @@
+package kuy_7.Maximum_Length_Difference
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class MaxDiffLengthTest {
+    @Test
+    fun test() {
+        println("mxdiflg Fixed Tests")
+        var s1 = arrayOf<String>("hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz")
+        var s2 = arrayOf<String>("cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww")
+        assertEquals(13,mxdiflg(s1, s2))
+        s1 = arrayOf<String>("ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh")
+        s2 = arrayOf<String>("bbbaaayddqbbrrrv")
+        assertEquals(10,mxdiflg(s1, s2))
+
+    }
+}
