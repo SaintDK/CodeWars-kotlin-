096bbb3b1d517fa7d14528aebc563f0794fc0941
ivany <ivan.yakimishin.00@mail.ru>
2023-04-09 22:56:42 +0300

    Commit

---
+++ src/main/kotlin/kuy_7/Alphabetical_Addition/TestExample.kt
@@ -0,0 +1,17 @@
+package kuy_7.Alphabetical_Addition
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class TestExample {
+    @Test
+    fun basicTests() {
+        assertEquals('f', addLetters(listOf('a', 'b', 'c')))
+        assertEquals('z', addLetters(listOf('z')))
+        assertEquals('c', addLetters(listOf('a', 'b')))
+        assertEquals('c', addLetters(listOf('c')))
+        assertEquals('a', addLetters(listOf('z', 'a')))
+        assertEquals('d', addLetters(listOf('y', 'c', 'b')))
+        assertEquals('z', addLetters(listOf()))
+    }
+}
---
+++ src/main/kotlin/kuy_7/Alphabetical_Addition/addLetters.kt
@@ -0,0 +1,11 @@
+package kuy_7.Alphabetical_Addition
+
+fun addLetters(arr: List<Char>): Char {
+
+    val alphabet = "zabcdefghijklmnopqrstuvwxy"
+    var sum = 0
+    for (str in arr) sum += str - 'a' + 1
+    return alphabet[sum % 26]
+
+
+}
