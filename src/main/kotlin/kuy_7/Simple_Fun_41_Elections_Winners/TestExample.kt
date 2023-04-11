74b774ca4299ea2178e886906849b22a79cc0d94
ivany <ivan.yakimishin.00@mail.ru>
2023-04-11 23:26:20 +0300

    Commit

---
+++ src/main/kotlin/kuy_7/Simple_Fun_41_Elections_Winners/TestExample.kt
@@ -0,0 +1,16 @@
+package kuy_7.Simple_Fun_41_Elections_Winners
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class TestExample {
+    @Test
+    fun `Basic Tests`() {
+        assertEquals(2, electionsWinners(arrayOf(2, 3, 5, 2), 3))
+        assertEquals(0, electionsWinners(arrayOf(1, 3, 3, 1, 1), 0))
+        assertEquals(1, electionsWinners(arrayOf(5, 1, 3, 4, 1), 0))
+        assertEquals(4, electionsWinners(arrayOf(1, 1, 1, 1), 1))
+        assertEquals(0, electionsWinners(arrayOf(1, 1, 1, 1), 0))
+        assertEquals(2, electionsWinners(arrayOf(3, 1, 1, 3, 1), 2))
+    }
+}
