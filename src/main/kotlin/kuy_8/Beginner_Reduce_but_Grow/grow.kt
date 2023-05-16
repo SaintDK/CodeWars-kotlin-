74009fecb60e2bd1680ccd4ccc2d4844e3daaeff
ivany <ivan.yakimishin.00@mail.ru>
2023-05-16 11:24:08 +0300

    Commit

--- src/main/kotlin/kuy_8/Beginner_Reduce_but_Grow/grow.kt
+++ src/main/kotlin/kuy_8/Beginner_Reduce_but_Grow/grow.kt
@@ -1,5 +1,11 @@
 package kuy_8.Beginner_Reduce_but_Grow
 
+//fun grow(arr: IntArray): Int {
+//    return arr.reduce {a, b -> a * b}
+//}
+
 fun grow(arr: IntArray): Int {
-    return arr.reduce {a, b -> a * b}
+    var result = 1
+    arr.forEach {result *= it}
+    return result
 }
