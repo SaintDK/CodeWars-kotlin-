5c11cf79fdeefd5a710a3b5586aeeeac956cb23c
ivany <ivan.yakimishin.00@mail.ru>
2023-06-25 19:56:05 +0300

    Commit

--- src/main/kotlin/kuy_7/Get_the_Middle_Character/getMiddle.kt
+++ src/main/kotlin/kuy_7/Get_the_Middle_Character/getMiddle.kt
@@ -1,6 +1,13 @@
 package kuy_7.Get_the_Middle_Character
 
+
+
 fun getMiddle(word : String) : String {
     val len = word.length
     return word.substring (len / 2 - (len + 1) % 2, len / 2 + 1)
 }
+
+//fun getMiddle(word : String) : String {
+//    if (word.length % 2 != 0) {return word[word.length / 2].toString() }
+//    return word[word.length / 2 - 1].toString() + word[word.length / 2 ].toString()
+//}
