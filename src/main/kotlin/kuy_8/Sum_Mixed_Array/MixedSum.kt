4ba6bb062b8ada37280011128b4d095e1c433088
ivany <ivan.yakimishin.00@mail.ru>
2023-06-29 13:10:14 +0300

    Commit

--- src/main/kotlin/kuy_8/Sum_Mixed_Array/MixedSum.kt
+++ src/main/kotlin/kuy_8/Sum_Mixed_Array/MixedSum.kt
@@ -1,16 +1,6 @@
 package kuy_8.Sum_Mixed_Array
-
 public class MixedSum {
-
     fun sum(list: List<Any>): Int {
-        var sum = 0
-        for (item in list) {
-            sum += when(item) {
-                is Int -> item
-                is String -> item.toInt()
-                else -> 0
-            }
-        }
-        return sum
+        return list.sumOf { (it as? String)?.toIntOrNull() ?: it as? Int ?: 0 }
     }
 }
