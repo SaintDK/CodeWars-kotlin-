3d7f3a25d52d5509479b5922e54a4059bea92dc0
ivany <ivan.yakimishin.00@mail.ru>
2023-07-08 15:55:49 +0300

    Commit

--- src/main/kotlin/kuy_8/Twice_as_old/TestExample.kt
+++ src/main/kotlin/kuy_8/Twice_as_old/TestExample.kt
@@ -4,3 +4,5 @@ fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int): Int {
     val diff = dadYearsOld - (sonYearsOld * 2)
     return Math.abs(diff)
 }
+
+//fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int): Int = Math.abs(dadYearsOld - (sonYearsOld * 2))
