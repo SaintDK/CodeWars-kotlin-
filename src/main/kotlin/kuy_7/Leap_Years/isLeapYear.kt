0a1d7da7b7ba1456ad13783e827d7f4f102dffdd
ivany <ivan.yakimishin.00@mail.ru>
2023-07-16 18:52:33 +0300

    Commit

--- src/main/kotlin/kuy_7/Leap_Years/isLeapYear.kt
+++ src/main/kotlin/kuy_7/Leap_Years/isLeapYear.kt
@@ -1,3 +1,11 @@
 package kuy_7.Leap_Years
 
 fun isLeapYear(year: Int) = java.time.Year.of(year).isLeap
+
+//fun isLeapYear(year: Int): Boolean {
+//    when {
+//        year % 400 == 0 -> return true
+//        year % 100 == 0 -> return false
+//        else -> return year % 4 == 0
+//    }
+//}
