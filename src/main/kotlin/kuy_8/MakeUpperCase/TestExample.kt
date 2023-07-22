74b2e2d353ef18c9b5324a6119e26dd629726e47
ivany <ivan.yakimishin.00@mail.ru>
2023-07-22 14:01:59 +0300

    Commit

---
+++ src/main/kotlin/kuy_8/MakeUpperCase/TestExample.kt
@@ -0,0 +1,11 @@
+package kuy_8.MakeUpperCase
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class TestExample {
+    @Test
+    fun testSomething() {
+        assertEquals("HELLO", makeUpperCase("hello"))
+    }
+}
---
+++ src/main/kotlin/kuy_8/MakeUpperCase/makeUpperCase.kt
@@ -0,0 +1,3 @@
+package kuy_8.MakeUpperCase
+
+fun makeUpperCase(str: String): String = str.toUpperCase()
