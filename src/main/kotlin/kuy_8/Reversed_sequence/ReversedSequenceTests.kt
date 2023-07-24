cdd5dbfc9853f0359e67291ca3217acc841dd025
ivany <ivan.yakimishin.00@mail.ru>
2023-07-24 16:21:14 +0300

    Commit

---
+++ src/main/kotlin/kuy_8/Reversed_sequence/ReversedSequenceTests.kt
@@ -0,0 +1,11 @@
+package kuy_8.Reversed_sequence
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ReversedSequenceTests {
+    @Test
+    fun exampleTestCases() {
+        assertEquals(listOf(5, 4, 3, 2, 1), reverseSeq(5))
+    }
+}
---
+++ src/main/kotlin/kuy_8/Reversed_sequence/reverseSeq.kt
@@ -0,0 +1,5 @@
+package kuy_8.Reversed_sequence
+
+fun reverseSeq(n: Int): List<Int> {
+    return (n downTo 1).toList()
+}
