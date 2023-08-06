73233699a2bdf22614bb0a03b73ac434253dcf24
ivany <ivan.yakimishin.00@mail.ru>
2023-08-06 09:30:18 -1200

    Commit

--- src/main/kotlin/kuy_7/ATM/count.kt
+++ src/main/kotlin/kuy_7/ATM/count.kt
@@ -7,3 +7,14 @@ fun count(amount: Int): Int {
     val (a, k) = values.fold(Pair(amount, 0)){ (a, k), v -> Pair(a % v, k + a / v) }
     return if (a == 0) k else -1
 }
+
+//fun count(number: Int):Int = when {
+//    number % 10 != 0 -> -1
+//    number - 500 >= 0 -> 1 + count(number - 500)
+//    number - 200 >= 0 -> 1 + count(number - 200)
+//    number - 100 >= 0 -> 1 + count(number - 100)
+//    number - 50 >= 0 -> 1 + count(number - 50)
+//    number - 20 >= 0 -> 1 + count(number - 20)
+//    number - 10 >= 0 -> 1 + count(number - 10)
+//    else -> 0
+//}
