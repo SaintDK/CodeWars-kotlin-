a67c10823187f1750fd769464da033a04b0562ff
ivany <ivan.yakimishin.00@mail.ru>
2023-08-08 12:28:22 +0300

    Commit

--- src/main/kotlin/kuy_8/Holiday_VIII_Duty_Free/TestExample.kt
+++ src/main/kotlin/kuy_8/Holiday_VIII_Duty_Free/TestExample.kt
@@ -1,8 +1,15 @@
 package kuy_8.Holiday_VIII_Duty_Free
 
-fun dutyFree(normPrice: Int, discount:Int, hol:Int): Int {
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
 
-    val answer = hol * 100 / (normPrice * discount)
-
-    return answer
+class TestExample {
+    @Test
+    fun `Basic tests`() {
+        assertEquals(166, dutyFree(12, 50, 1000))
+        assertEquals(294, dutyFree(17, 10, 500))
+        assertEquals(357, dutyFree(24, 35, 3000))
+        assertEquals(60, dutyFree(377, 40, 9048))
+        assertEquals(10, dutyFree(2479, 51, 13390))
+    }
 }
--- src/main/kotlin/kuy_8/Holiday_VIII_Duty_Free/dutyFree.kt
+++ src/main/kotlin/kuy_8/Holiday_VIII_Duty_Free/dutyFree.kt
@@ -1,15 +1,10 @@
 package kuy_8.Holiday_VIII_Duty_Free
 
-import junit.framework.TestCase.assertEquals
-import org.junit.Test
+fun dutyFree(normPrice: Int, discount:Int, hol:Int): Int {
 
-class TestExample {
-    @Test
-    fun `Basic tests`() {
-        assertEquals(166, dutyFree(12, 50, 1000))
-        assertEquals(294, dutyFree(17, 10, 500))
-        assertEquals(357, dutyFree(24, 35, 3000))
-        assertEquals(60, dutyFree(377, 40, 9048))
-        assertEquals(10, dutyFree(2479, 51, 13390))
-    }
+    val answer = hol * 100 / (normPrice * discount)
+
+    return answer
 }
+
+//fun dutyFree(normPrice: Int, discount:Int, hol:Int) = hol * 100 / (normPrice * discount)
