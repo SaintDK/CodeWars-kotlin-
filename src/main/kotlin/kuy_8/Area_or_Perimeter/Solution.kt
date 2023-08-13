bc46f1265e25fe0b0ebe239cf7dcf98fec3a5755
ivany <ivan.yakimishin.00@mail.ru>
2023-08-13 13:54:42 +0300

    Commit

--- src/main/kotlin/kuy_8/Area_or_Perimeter/Solution.kt
+++ src/main/kotlin/kuy_8/Area_or_Perimeter/Solution.kt
@@ -1,7 +1,11 @@
 package kuy_8.Area_or_Perimeter
 
 object Solution {
-    fun areaOrPerimeter(l:Int, w:Int):Int {
-        return l*w;
+    fun areaOrPerimeter(l: Int, w: Int): Int {
+        return if (l == w) {
+            l * l // area of square
+        } else {
+            2 * l + 2 * w // perimeter of rectangle
+        }
     }
 }
