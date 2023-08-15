745a2ec1c9c1d6a64ad4f1af10b92d92db4d246e
ivany <ivan.yakimishin.00@mail.ru>
2023-08-15 18:12:46 +0300

    Commit

--- src/main/kotlin/kuy_7/Decimal_Time_Conversion/toIndustrial.kt
+++ src/main/kotlin/kuy_7/Decimal_Time_Conversion/toIndustrial.kt
@@ -9,21 +9,6 @@ fun toNormal(t: Double): String {
     return "%d:%02d".format(time / 60, time % 60)
 }
 
-//fun toIndustrial(t: Int): Double{
-//    return (t*60.0/36.0).roundToInt()/100.0
-//}
-//
-//fun toIndustrial(t: String): Double{
-//    val h = t.split(":")[0].toInt()
-//    val m = t.split(":")[1].toInt()
-//    return ((h*60+m)*60.0/36.0).roundToInt()/100.0
-//}
-//
-//fun toNormal(t: Double): String{
-//    val h = t.toInt()
-//    val m = t-t.toInt()
-//    return h.toString() + ":" + (m*60.0).roundToInt().toString().padStart(2,'0')
-//}
 
 //fun toIndustrial(t: Int): Double = (t * 100 / 60.0).roundToInt() / 100.0
 //
