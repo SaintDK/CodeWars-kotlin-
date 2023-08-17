27c08b5878ee704c66f561a8af13a40b3a6acaf2
ivany <ivan.yakimishin.00@mail.ru>
2023-08-17 13:08:16 +0300

    Commit

--- src/main/kotlin/kuy_7/Log_without_dates/checkLogs.kt
+++ src/main/kotlin/kuy_7/Log_without_dates/checkLogs.kt
@@ -2,3 +2,6 @@ package kuy_7.Log_without_dates
 
 fun checkLogs(log: Array<String>): Int =
     log.size - log.asSequence().zipWithNext{ x, y -> x < y }.count{ it }
+
+//fun checkLogs(log: Array<String>): Int =
+//    log.toList().zipWithNext().count {(a, b) -> b <= a} + (if (log.size == 0) 0 else 1)
