cc7327f02eacb1c8c16de4f78f3c07f4e32278c6
ivany <ivan.yakimishin.00@mail.ru>
2023-08-20 23:31:06 +0300

    Commit

--- src/main/kotlin/kuy_7/Log_without_dates/checkLogs.kt
+++ src/main/kotlin/kuy_7/Log_without_dates/checkLogs.kt
@@ -5,3 +5,13 @@ fun checkLogs(log: Array<String>): Int =
 
 //fun checkLogs(log: Array<String>): Int =
 //    log.toList().zipWithNext().count {(a, b) -> b <= a} + (if (log.size == 0) 0 else 1)
+
+//fun checkLogs(log: Array<String>): Int {
+//    var c = 0
+//    if (log.size > 0) c++
+//    val x = List(log.size) { log[it].split(":").joinToString("").toInt() }
+//    for (a in 1..x.size - 1) {
+//        if (x[a] <= x[a - 1]) c++
+//    }
+//    return c
+//}
