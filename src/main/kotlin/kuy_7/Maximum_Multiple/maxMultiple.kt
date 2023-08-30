b5d7911bfa2540b9a70591028fe87cfb6a7c1f8b
ivany <ivan.yakimishin.00@mail.ru>
2023-08-30 09:01:01 +0300

    Commit

--- src/main/kotlin/kuy_7/Maximum_Multiple/maxMultiple.kt
+++ src/main/kotlin/kuy_7/Maximum_Multiple/maxMultiple.kt
@@ -23,3 +23,6 @@ fun maxMultiple(d: Int, b: Int): Int = (b downTo d).first { it % d == 0 }
 //    (b.toFloat() - b.toFloat().mod(d.toFloat())).toInt()
 
 //fun maxMultiple(d: Int, b: Int): Int? = (1..b).filter{it%d == 0 }.maxOrNull()
+
+//tailrec fun maxMultiple(d: Int, b: Int): Int =
+//    if (b % d == 0) b else maxMultiple(d,b-1)
